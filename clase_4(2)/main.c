96750aad4437f1b1f031ec9c61c1584447db50d1
unknown <lodolatomas@github.com>
2019-09-02 12:09:57 -0300

    mas cosas de punteros

--- clase_4(2)/main.c
+++ clase_4(2)/main.c
@@ -20,7 +20,48 @@ int main()
 
 
     printf("Ud ingreso: %d----%d",legajo,nota);
+/*
 
-    return 0;
+pasaje por valor, pasamos varibales de tipo entero(int) char y float
+pasaje por referencias, pasamos arrays y punteros
+
+main{
+
+int numero=4;
+fx(numero);
+***en el caso de querer cambiar numero tendriamos que hacer
+***numero = fx(numero);
+
 }
 
+fx(int x)//x tiene una copia de numero
+{
+
+x=9// solo se modifica x a noser
+***x=9
+***return x;
+
+}
+
+
+*/
+    return 0;
+}
+/*                PUNTEROS (BASICO)
+    main
+    {
+
+    int numero=4;
+    fx(&numero);//envio la direccion de memoria
+
+    }
+
+    fx(int* x)
+    {
+
+    *x=9;//modifico directamente al valor de numero
+    //modifica al valor que apunta
+    }
+
+
+*/
